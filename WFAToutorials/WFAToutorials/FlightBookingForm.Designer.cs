552a27f6323e7d566bc9339e2e5c514e7ea488a0
ShahriyarH10 <md.shahriyarhasan2002me@gmail.com>
2025-06-10 15:38:23 +0600

    Add travel document fields and logic to booking form
    Updated `FlightBookingForm.Designer.cs` to include new UI elements for first name, last name, document number, and travel document selection (Passport/ID Card). Adjusted layout and form size to accommodate changes.
    
    Modified `FlightBookingForm.cs` to add event handlers for radio buttons and implement logic for document selection. Added validation to ensure expiry date cannot precede issue date.

--- WFAToutorials/WFAToutorials/FlightBookingForm.Designer.cs
+++ WFAToutorials/WFAToutorials/FlightBookingForm.Designer.cs
@@ -33,15 +33,35 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.txtDocumentNo = new System.Windows.Forms.TextBox();
+            this.lblDocumentNo = new System.Windows.Forms.Label();
+            this.rbtnPassport = new System.Windows.Forms.RadioButton();
+            this.rbtnIDCard = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblIssueDate = new System.Windows.Forms.Label();
+            this.dtpIssueDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpExpiryDate = new System.Windows.Forms.DateTimePicker();
+            this.lblExpiryDate = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(95, 27);
+            this.label1.Location = new System.Drawing.Point(142, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.Size = new System.Drawing.Size(45, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "To:";
             // 
@@ -49,49 +69,246 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(398, 27);
+            this.label2.Location = new System.Drawing.Point(548, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.Size = new System.Drawing.Size(68, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "From:";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(131, 24);
+            this.textBox1.Location = new System.Drawing.Point(196, 37);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(148, 30);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(450, 24);
+            this.textBox2.Location = new System.Drawing.Point(626, 32);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(148, 30);
             this.textBox2.TabIndex = 3;
             // 
             // monthCalendar1
             // 
             this.monthCalendar1.AllowDrop = true;
             this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 1);
-            this.monthCalendar1.Location = new System.Drawing.Point(98, 58);
+            this.monthCalendar1.Location = new System.Drawing.Point(147, 81);
+            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(14);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.SelectionRange = new System.Windows.Forms.SelectionRange(new System.DateTime(2025, 5, 24, 0, 0, 0, 0), new System.DateTime(2025, 5, 30, 0, 0, 0, 0));
             this.monthCalendar1.TabIndex = 4;
             // 
+            // txtLName
+            // 
+            this.txtLName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLName.Location = new System.Drawing.Point(873, 355);
+            this.txtLName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(148, 30);
+            this.txtLName.TabIndex = 8;
+            // 
+            // txtFName
+            // 
+            this.txtFName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFName.Location = new System.Drawing.Point(282, 356);
+            this.txtFName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(148, 30);
+            this.txtFName.TabIndex = 7;
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLName.Location = new System.Drawing.Point(735, 358);
+            this.lblLName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(122, 25);
+            this.lblLName.TabIndex = 6;
+            this.lblLName.Text = "Last Name:";
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFName.Location = new System.Drawing.Point(148, 356);
+            this.lblFName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(123, 25);
+            this.lblFName.TabIndex = 5;
+            this.lblFName.Text = "First Name:";
+            // 
+            // txtDocumentNo
+            // 
+            this.txtDocumentNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDocumentNo.Location = new System.Drawing.Point(872, 408);
+            this.txtDocumentNo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDocumentNo.Name = "txtDocumentNo";
+            this.txtDocumentNo.Size = new System.Drawing.Size(148, 30);
+            this.txtDocumentNo.TabIndex = 10;
+            // 
+            // lblDocumentNo
+            // 
+            this.lblDocumentNo.AutoSize = true;
+            this.lblDocumentNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDocumentNo.Location = new System.Drawing.Point(718, 411);
+            this.lblDocumentNo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDocumentNo.Name = "lblDocumentNo";
+            this.lblDocumentNo.Size = new System.Drawing.Size(149, 25);
+            this.lblDocumentNo.TabIndex = 9;
+            this.lblDocumentNo.Text = "Document No:";
+            // 
+            // rbtnPassport
+            // 
+            this.rbtnPassport.AutoSize = true;
+            this.rbtnPassport.Location = new System.Drawing.Point(9, 6);
+            this.rbtnPassport.Name = "rbtnPassport";
+            this.rbtnPassport.Size = new System.Drawing.Size(97, 24);
+            this.rbtnPassport.TabIndex = 11;
+            this.rbtnPassport.TabStop = true;
+            this.rbtnPassport.Text = "Passport";
+            this.rbtnPassport.UseVisualStyleBackColor = true;
+            this.rbtnPassport.CheckedChanged += new System.EventHandler(this.rbtnPassport_CheckedChanged);
+            // 
+            // rbtnIDCard
+            // 
+            this.rbtnIDCard.AutoSize = true;
+            this.rbtnIDCard.Location = new System.Drawing.Point(133, 3);
+            this.rbtnIDCard.Name = "rbtnIDCard";
+            this.rbtnIDCard.Size = new System.Drawing.Size(89, 24);
+            this.rbtnIDCard.TabIndex = 12;
+            this.rbtnIDCard.TabStop = true;
+            this.rbtnIDCard.Text = "ID Card";
+            this.rbtnIDCard.UseVisualStyleBackColor = true;
+            this.rbtnIDCard.CheckedChanged += new System.EventHandler(this.rbtnIDCard_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rbtnPassport);
+            this.panel1.Controls.Add(this.rbtnIDCard);
+            this.panel1.Location = new System.Drawing.Point(333, 408);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(225, 35);
+            this.panel1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(142, 412);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Travel Document:";
+            // 
+            // lblIssueDate
+            // 
+            this.lblIssueDate.AutoSize = true;
+            this.lblIssueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIssueDate.Location = new System.Drawing.Point(142, 464);
+            this.lblIssueDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIssueDate.Name = "lblIssueDate";
+            this.lblIssueDate.Size = new System.Drawing.Size(225, 25);
+            this.lblIssueDate.TabIndex = 15;
+            this.lblIssueDate.Text = "Document Issue Date:";
+            // 
+            // dtpIssueDate
+            // 
+            this.dtpIssueDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpIssueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpIssueDate.Location = new System.Drawing.Point(378, 464);
+            this.dtpIssueDate.Name = "dtpIssueDate";
+            this.dtpIssueDate.Size = new System.Drawing.Size(199, 28);
+            this.dtpIssueDate.TabIndex = 16;
+            this.dtpIssueDate.ValueChanged += new System.EventHandler(this.dtpIssueDate_ValueChanged);
+            // 
+            // dtpExpiryDate
+            // 
+            this.dtpExpiryDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpExpiryDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpExpiryDate.Location = new System.Drawing.Point(820, 461);
+            this.dtpExpiryDate.Name = "dtpExpiryDate";
+            this.dtpExpiryDate.Size = new System.Drawing.Size(201, 28);
+            this.dtpExpiryDate.TabIndex = 18;
+            // 
+            // lblExpiryDate
+            // 
+            this.lblExpiryDate.AutoSize = true;
+            this.lblExpiryDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExpiryDate.Location = new System.Drawing.Point(584, 464);
+            this.lblExpiryDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblExpiryDate.Name = "lblExpiryDate";
+            this.lblExpiryDate.Size = new System.Drawing.Size(233, 25);
+            this.lblExpiryDate.TabIndex = 17;
+            this.lblExpiryDate.Text = "Document Expiry Date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(142, 510);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 25);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Baggage:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(249, 512);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 20;
+            // 
+            // btnBook
+            // 
+            this.btnBook.BackColor = System.Drawing.Color.Brown;
+            this.btnBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBook.Location = new System.Drawing.Point(12, 628);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(1176, 52);
+            this.btnBook.TabIndex = 21;
+            this.btnBook.Text = "Book(without paying)";
+            this.btnBook.UseVisualStyleBackColor = false;
+            // 
             // FlightBookingForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtpExpiryDate);
+            this.Controls.Add(this.lblExpiryDate);
+            this.Controls.Add(this.dtpIssueDate);
+            this.Controls.Add(this.lblIssueDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtDocumentNo);
+            this.Controls.Add(this.lblDocumentNo);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.lblFName);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FlightBookingForm";
             this.Text = "FlightBookingForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,5 +321,22 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.TextBox txtDocumentNo;
+        private System.Windows.Forms.Label lblDocumentNo;
+        private System.Windows.Forms.RadioButton rbtnPassport;
+        private System.Windows.Forms.RadioButton rbtnIDCard;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblIssueDate;
+        private System.Windows.Forms.DateTimePicker dtpIssueDate;
+        private System.Windows.Forms.DateTimePicker dtpExpiryDate;
+        private System.Windows.Forms.Label lblExpiryDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnBook;
     }
 }
--- WFAToutorials/WFAToutorials/FlightBookingForm.cs
+++ WFAToutorials/WFAToutorials/FlightBookingForm.cs
@@ -12,9 +12,43 @@ namespace WFAToutorials
 {
     public partial class FlightBookingForm : Form
     {
+
+        public static Boolean Passport, IDcard;
         public FlightBookingForm()
         {
             InitializeComponent();
+        }
+
+        private void rbtnIDCard_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnIDCard.Checked)
+            {
+                lblDocumentNo.Text = "ID Card No.";
+                lblIssueDate.Text = "Issue Date";
+                lblExpiryDate.Text = "Expiry Date";
+
+
+                IDcard = true;
+            }
+        }
+
+        private void dtpIssueDate_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime dt = dtpIssueDate.Value;
+            dtpExpiryDate.MinDate = dt;
+        }
+
+        private void rbtnPassport_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnPassport.Checked)
+            {
+                lblDocumentNo.Text = "Passport No.";
+                lblIssueDate.Text = "Issue Date";
+                lblExpiryDate.Text = "Expiry Date";
+
+
+                Passport = true;
+            }
         }
     }
 }
